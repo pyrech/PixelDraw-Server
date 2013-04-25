66befd4b578cf574abcb1de19273f9f90df66608
Loick Piera <pyrech@gmail.com>
2013-04-25 09:51:29 +0200

    logs are now write in a file - Part 4

--- socket/run.php
+++ socket/run.php
@@ -4,13 +4,10 @@ use Ratchet\Wamp\WampServer;
 use Ratchet\Server\IoServer;
 use Ratchet\WebSocket\WsServer;
 
-define('PD_SOCKET_ROOT', __DIR__);
-
+require __DIR__. '/conf/system.php';
 require PD_SOCKET_ROOT . '/conf/log.php';
 require PD_SOCKET_ROOT . '/conf/database.php';
 require PD_SOCKET_ROOT . '/vendor/autoload.php';
-
-system('tail -f '.PD_LOG_FILE);
 
 $pd_server = new PixelDraw\Server();
 
