9d7b84e461f3a19f9f4ad06fb19434b471a5b183
Loick Piera <pyrech@gmail.com>
2013-04-25 09:08:45 +0200

    Fix non static access to static members in Words

--- socket/src/PixelDraw/Words.php
+++ socket/src/PixelDraw/Words.php
@@ -10,28 +10,28 @@ class Words {
   private static $query_word = null;
 
   public static function collectCategories(\PDO $pdo, $limit) {
-    if ($this->query_collect == null) {
-      $this->query_collect == $pdo->prepare('SELECT * FROM category LIMIT '.intval($limit).' ORDER BY RAND( )');
+    if (self::$query_collect == null) {
+      self::$query_collect == $pdo->prepare('SELECT * FROM category LIMIT '.intval($limit).' ORDER BY RAND( )');
     }
-    $this->query_collect->exec();
-    return $this->query_collect->fetch_all();
+    self::$query_collect->exec();
+    return self::$query_collect->fetch_all();
   }
 
   public static function existsCategory(\PDO $pdo, $category_id) {
-    if ($this->query_exists == null) {
-      $this->query_exists == $pdo->prepare('SELECT COUNT(*) as count FROM category WHERE id = :id');
+    if (self::$query_exists == null) {
+      self::$query_exists == $pdo->prepare('SELECT COUNT(*) as count FROM category WHERE id = :id');
     }
-    $this->query_exists->exec(array(':id' => intval($category_id)));
-    $res = $this->query_exists->fetch();
+    self::$query_exists->exec(array(':id' => intval($category_id)));
+    $res = self::$query_exists->fetch();
     return $res['count'] > 0;
   }
 
   public static function getRandomWord(\PDO $pdo, $category_id) {
-    if ($this->query_word == null) {
-      $this->query_word == $pdo->prepare('SELECT * as count FROM word WHERE category_id = :category_id LIMIT 1 ORDER BY RAND( )');
+    if (self::$query_word == null) {
+      self::$query_word == $pdo->prepare('SELECT * as count FROM word WHERE category_id = :category_id LIMIT 1 ORDER BY RAND( )');
     }
-    $this->query_word->exec(array(':category_id' => intval($category_id)));
-    return $this->query_word->fetch();
+    self::$query_word->exec(array(':category_id' => intval($category_id)));
+    return self::$query_word->fetch();
   }
 
 }
