36728c1dd01c92aed65237eac475e96406e02c6f
Loick Piera <pyrech@gmail.com>
2013-04-25 13:16:24 +0200

    fix word and category

--- socket/src/PixelDraw/Words.php
+++ socket/src/PixelDraw/Words.php
@@ -10,16 +10,16 @@ class Words {
   private static $query_word = null;
 
   public static function collectCategories(\PDO $pdo, $limit) {
-    if (self::$query_collect == null) {
-      self::$query_collect == $pdo->prepare('SELECT * FROM category LIMIT '.intval($limit).' ORDER BY RAND( )');
+    if (empty(self::$query_collect)) {
+      self::$query_collect = $pdo->prepare('SELECT * FROM category LIMIT '.intval($limit).' ORDER BY RAND( )');
     }
     self::$query_collect->exec();
     return self::$query_collect->fetch_all();
   }
 
   public static function existsCategory(\PDO $pdo, $category_id) {
-    if (self::$query_exists == null) {
-      self::$query_exists == $pdo->prepare('SELECT COUNT(*) as count FROM category WHERE id = :id');
+    if (empty(self::$query_exists)) {
+      self::$query_exists = $pdo->prepare('SELECT COUNT(*) as count FROM category WHERE id = :id');
     }
     self::$query_exists->exec(array(':id' => intval($category_id)));
     $res = self::$query_exists->fetch();
@@ -27,8 +27,8 @@ class Words {
   }
 
   public static function getRandomWord(\PDO $pdo, $category_id) {
-    if (self::$query_word == null) {
-      self::$query_word == $pdo->prepare('SELECT * as count FROM word WHERE category_id = :category_id LIMIT 1 ORDER BY RAND( )');
+    if (empty(self::$query_word)) {
+      self::$query_word = $pdo->prepare('SELECT * as count FROM word WHERE category_id = :category_id LIMIT 1 ORDER BY RAND( )');
     }
     self::$query_word->exec(array(':category_id' => intval($category_id)));
     return self::$query_word->fetch();
